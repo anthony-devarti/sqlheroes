95252309c563261ec0d8c413eb4e3c469388939a
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-04-05 21:40:35 +0000

    setting up structure

--- demo.py
+++ demo.py
@@ -6,6 +6,65 @@ select_heroes = """
     ORDER BY id DESC 
 """
 
-heroes = execute_query(select_heroes).fetchall()
-for hero in heroes:
-    print(hero[1])
+# heroes = execute_query(select_heroes).fetchall()
+# for hero in heroes:
+#     print(hero[1])
+
+# select_a_hero = """
+#     SELECT name, about_me FROM heroes
+#     WHERE name = 'The Seer'
+# """
+
+# hero = execute_query(select_a_hero).fetchall()
+# print(hero)
+
+
+#####################################
+##searching for a precise hero name##
+#####################################
+def find_hero(name):
+            select_a_hero = """
+            SELECT name, about_me FROM heroes
+            WHERE name = '{}'
+            """.format(name)
+            hero = execute_query(select_a_hero).fetchall()
+            print(hero)
+
+
+
+#this should add a hero.  It is working when things are hard-coded into the add_a_hero string, but not when they are placed in as variables
+def add_hero(name, about_string, biography):
+    about = about_string
+    add_a_hero="""
+    INSERT INTO heroes (name, about_me, biography)
+    VALUES ({0}, {1}, {2})
+    """.format(name, about_string, biography)
+    add=execute_query(add_a_hero)
+    print(add)
+
+# add_hero('Quincy', 'no idea', 'He forgot his keys')
+
+
+############################################################################################################
+## The whole program should run inside of this function, and allow users to type in what they want to do" ##
+############################################################################################################
+
+def initialize(instruction):
+    if instruction == 'find' or 'Find':
+        print('What Hero are you looking for? ')
+        x=input()
+        find_hero(str(x))
+    elif instruction == 'Create' or 'create':
+        print('Tell me about your hero! \n What\'s their name? ')
+        name=input()
+        print('That\'s a great name!  What would they say about themselves?')
+        about_string=input()
+        print('I bet they would say that.  What\'s their bio?')
+        bio=input()
+        add_hero(name, about_string, biography)
+        
+print(
+    'Instructions\n What do you want to do?\n Find: Type a Hero\'s name to learn more about them \n Create: Make up your own hero.'
+)
+instruction = input()
+initialize(instruction)
---
+++ tests/test_1.py
@@ -0,0 +1,12 @@
+import pytest
+###something is going wrong here and I cannot figure out what it is.  Bring up as a blocker
+# from demo import find_hero
+
+## get tests set up correctly
+def test_always_passes():
+
+    assert True
+
+#test
+def test_search():
+    assert demo.find_hero('The Hummingbird') == "It's a Bird! It's a plane! Oh wait, it's really just a bird... It's The Hummingbird!"
