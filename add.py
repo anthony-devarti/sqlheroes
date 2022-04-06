cbdb9537d38e534bdf7b6f3323c40a0f4413ff24
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-04-06 01:32:49 +0000

    added comments

--- add.py
+++ add.py
@@ -1,5 +1,7 @@
 from connection import execute_query
 
+#leaving this so I can go back over the process of solving this
+
 #this is working if I hard code the values, but when I try to add them as variables, I am getting the error:
 #name 'Error' is not defined
 #maybe this has to do with the formatting of the variables?
@@ -10,6 +12,10 @@ from connection import execute_query
 #get the 3 fields to dynamically fill :done:
 
 #get the fields filled by input that is passed in via a few questions.
+
+#######################
+## Adding a New Hero ##
+#######################
 
 def add_hero(hero, about, bio):
     add_a_hero="""
